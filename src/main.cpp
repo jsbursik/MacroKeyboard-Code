af3dcc5d0233c9f826b5a9f8aabae01b1d4c88bf
Jordan <jsbursik@gmail.com>
2021-01-09 23:44:02 -0600

    New scrolling implementation

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,10 @@
 
 // Setup OLED
 Adafruit_SSD1306 display(128, 32, &Wire, 13);
+String textBuffer = "";
+
+// Scroll Text setup
+int x, x2, minX;
 
 // Mode Button Variables
 int modePushCounter = 0;
@@ -42,12 +46,15 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 // Function Declarations
 void checkModeButton();
 void writeToDisplay(String toWrite, int size);
+void scrollText(String toWrite, int size);
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  writeToDisplay("Macro Keyboard v2", 1);
+  display.setTextWrap(false);
+  x = display.width();
+  writeToDisplay("Powering on...", 2);
   delay(3000);
-
+  
   pinMode(ModeButton, INPUT_PULLUP);
   Keyboard.begin();
 
@@ -60,7 +67,7 @@ void loop() {
   checkModeButton();
   switch(modePushCounter){
     case 0:
-      writeToDisplay("General", 2);
+      scrollText("General", 4);
       switch(key){
         case '1': 
           // Discord Mute Microphone
@@ -110,10 +117,10 @@ void loop() {
           // 
           break;
       }
-      delay(50); Keyboard.releaseAll();
+      delay(10); Keyboard.releaseAll();
       break;
     case 1:
-      writeToDisplay("VSCode", 2);
+      scrollText("VSCode", 4);
       switch(key){
         case '1':
           // Open Command Panel
@@ -179,28 +186,76 @@ void loop() {
           Keyboard.press(KEY_END);
           break;
       }
-      delay(50); Keyboard.releaseAll();
+      delay(10); Keyboard.releaseAll();
       break;
     case 2:
-      writeToDisplay("Mode 2", 2);
+      scrollText("World of Warcraft", 4);
       switch(key){
-        case '1': Keyboard.println("Mode 2, key 2"); break;
-        case '2': Keyboard.println("Mode 2, key 1"); break;
-        case '3': Keyboard.println("Mode 2, key 3"); break;
-        case '4': Keyboard.println("Mode 2, key 4"); break;
-        case '5': Keyboard.println("Mode 2, key 5"); break;
-        case '6': Keyboard.println("Mode 2, key 6"); break;
-        case '7': Keyboard.println("Mode 2, key 7"); break;
-        case '8': Keyboard.println("Mode 2, key 8"); break;
-        case '9': Keyboard.println("Mode 2, key 9"); break;
-        case '0': Keyboard.println("Mode 2, key 0"); break;
-        case 'A': Keyboard.println("Mode 2, key A"); break;
-        case 'B': Keyboard.println("Mode 2, key B"); break;
+        case '1': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('1');
+          break;
+        case '2': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('2');
+          break;
+        case '3': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('3');
+          break;
+        case '4': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('4');
+          break;
+        case '5': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('5');
+          break;
+        case '6': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('6');
+          break;
+        case '7': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('7');
+          break;
+        case '8': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('8');
+          break;
+        case '9': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('8');
+          break;
+        case '0': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('0');
+          break;
+        case 'A': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('-');
+          break;
+        case 'B': 
+          Keyboard.press(CTRL);
+          Keyboard.press(SHIFT);
+          Keyboard.print('=');
+          break;
       }
-      delay(50); Keyboard.releaseAll();
+      delay(10); Keyboard.releaseAll();
       break;
     case 3:
-      writeToDisplay("Mode 3", 2);
+      scrollText("Mode 3", 4);
       switch(key){
         case '1': Keyboard.println("Mode 3, key 1"); break;
         case '2': Keyboard.println("Mode 3, key 2"); break;
@@ -215,19 +270,42 @@ void loop() {
         case 'A': Keyboard.println("Mode 3, key A"); break;
         case 'B': Keyboard.println("Mode 3, key B"); break;
       }
-      delay(50); Keyboard.releaseAll();
+      delay(10); Keyboard.releaseAll();
       break;
   }
   delay(1);
 }
 
 void writeToDisplay(String toWrite, int size){
+  if(toWrite != textBuffer){
+    display.clearDisplay();
+    display.setCursor(0,12);
+    display.setTextSize(size);
+    display.setTextColor(SSD1306_WHITE);
+    display.print(toWrite);
+    display.display();
+  }
+  textBuffer = toWrite;
+}
+
+void scrollText(String toWrite, int size){
+  #define SPACER  2 * size * 6;
+  if(toWrite != textBuffer){
+    x = display.width();
+    x2 = x + (toWrite.length() * size * 6) + SPACER;
+  }
+  textBuffer = toWrite;
   display.clearDisplay();
-  display.setCursor(0,12);
   display.setTextSize(size);
   display.setTextColor(SSD1306_WHITE);
+  display.setCursor(x, 0);
+  display.print(toWrite);
+  display.setCursor(x2, 0);
   display.print(toWrite);
   display.display();
+  minX = -6 * size * toWrite.length();
+  if(--x < minX) x = x2 + (toWrite.length() * size * 6) + SPACER;
+  if(--x2 < minX) x2 = x + (toWrite.length() * size * 6) + SPACER;
 }
 
 void checkModeButton(){
